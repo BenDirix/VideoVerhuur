1fd3de4e88da358ad865cf41d05ca9632a37b237
Benjamin Dirix <BenjaminDirix@hotmail.com>
2019-04-23 11:31:17 +0200

    filmdetail

--- VideoVerhuur/Controllers/VerhuurController.cs
+++ VideoVerhuur/Controllers/VerhuurController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoVerhuur.Services;
+using VideoVerhuur.Models;
 
 namespace VideoVerhuur.Controllers
 {
     public class VerhuurController : Controller
     {
+        private VideoVerhuurService _dbContext = new VideoVerhuurService();
         // GET: Verhuur
         public ActionResult Index()
         {
-            return View();
+            var genres = _dbContext.GetGenres();
+            if(Session["klant"] == null)
+                return RedirectToAction("Index", "Home");
+            return View(genres);
+        }
+
+        public ActionResult FilmDetails(int genreId)
+        {
+            var filmsVM = new FilmDetailViewModel
+            {
+                Films = _dbContext.GetFilms(genreId),
+                Genre = _dbContext.GetGenre(genreId)
+            };
+            return View(filmsVM);
         }
     }
 }
--- VideoVerhuur/Services/VideoVerhuurService.cs
+++ VideoVerhuur/Services/VideoVerhuurService.cs
@@ -15,5 +15,27 @@ namespace VideoVerhuur.Services
                 return db.Klanten.Where(k => k.Naam == naam && k.PostCode == postcode).FirstOrDefault();
             }
         }
+
+        public List<Genre> GetGenres()
+        {
+            using(var db = new VideoVerhuurEntities())
+            {
+                return db.Genres.ToList();
+            }
+        }
+        public Genre GetGenre(int Id)
+        {
+            using(var db = new VideoVerhuurEntities())
+            {
+                return db.Genres.SingleOrDefault(g => g.GenreNr == Id);
+            }
+        }
+        public List<Film> GetFilms(int genreId)
+        {
+            using(var db = new VideoVerhuurEntities())
+            {
+                return db.Films.Where(f => f.GenreNr == genreId).OrderBy(f => f.Titel).ToList();
+            }
+        }
     }
 }
