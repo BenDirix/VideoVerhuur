32691fcfaaf50eecc58fdff00651733f8d974b2d
Benjamin Dirix <BenjaminDirix@hotmail.com>
2019-04-23 10:14:04 +0200

    init

--- VideoVerhuur/Controllers/HomeController.cs
+++ VideoVerhuur/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoVerhuur.Models;
+using VideoVerhuur.Services;
 
 namespace VideoVerhuur.Controllers
 {
     public class HomeController : Controller
     {
+        private VideoVerhuurService _dbContext = new VideoVerhuurService();
+        public HomeController()
+        {
+
+        }
         public ActionResult Index()
         {
+            Session.Clear();
             return View();
         }
 
@@ -25,6 +33,15 @@ namespace VideoVerhuur.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        
+        public ActionResult Bevestiging(string naam, int postcode)
+        {
+            var klant = _dbContext.GetKlant(naam.ToUpper(), postcode);
+            Session["klant"] = klant;
+            if(klant == null)
+                return View("Foutief");
+            return View("Welkom");
         }
     }
 }
---
+++ VideoVerhuur/Services/VideoVerhuurService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VideoVerhuur.Models;
+
+namespace VideoVerhuur.Services
+{
+    public class VideoVerhuurService
+    {
+        public Klant GetKlant(string naam, int postcode)
+        {
+            using(var db = new VideoVerhuurEntities())
+            {
+                return db.Klanten.Where(k => k.Naam == naam && k.PostCode == postcode).FirstOrDefault();
+            }
+        }
+    }
+}
